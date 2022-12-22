870ba156f7aeae1cfa08101b4ccdae10c8148b8f
Alex Andrieiev <andryeyevalex@gmail.com>
2022-12-22 03:41:59 +0200

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -4,16 +4,6 @@ import { getFilter } from 'redux/filterSlice';
 import { Contacts } from './ContactsList.styled';
 import { ContactsItem } from 'components/ContactsListItem/ContactsListItem';
 
-// const getFilteredContacts = (contacts, filter) => {
-//   if (filter === '') {
-//     return contacts;
-//   }
-
-//   return contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(filter.toLowerCase())
-//   );
-// };
-
 export const ContactsList = () => {
   const filter = useSelector(getFilter);
   const contacts = useSelector(getContacts);
